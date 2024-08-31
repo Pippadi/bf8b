8e30ed7676bd379006c41fd863e15505de5614c1
Prithvi Vishak <prithvivishak@gmail.com>
2024-08-31 13:11:00 +0530

    Why was the last commit so broken?

--- 8bit.sv
+++ 8bit.sv
@@ -12,39 +12,48 @@ module eightbit(
     reg [3:0] inst_stage;
     reg alu_do;
 
+    alu ALU(.r1(a), .r2(b), .inst(inst), .en(alu_do));
+
     initial begin
-        a = 8'h00;
-        b = 8'h00;
         pc = 8'h00;
         inst = 8'h00;
-        pipe = 3'h0;
-        inst_stage = 4'h00;
+        inst_stage = 4'h0;
         we = 1'b0;
         alu_do = 0;
         addr = pc;
     end
 
-    alu ALU(.r1(a), .r2(b), .inst(inst), .en(alu_do));
-
     endmodule
 
     module alu(
-        input reg [7:0] r1,
-        input reg [7:0] r2,
+        input en,
         input reg [7:0] inst,
-        input en
+        input reg [7:0] r2,
+        output reg [7:0] r1
         );
+
         always @ (posedge en) begin
-            case (instr[7:0])
-                8'h10: a = a + b;
-                8'h11: a = a - b;
-                8'h12: a = a * b;
-                8'h13: a = a / b;
-                8'h14: a = a & b;
-                8'h15: a = a | b;
-                8'h16: a = a ^ b;
-                8'h17: a = ~a;
+            case (inst)
+                8'h10: r1 = r1 + r2;
+                8'h11: r1 = r1 - r2;
+                8'h12: r1 = r1 * r2;
+                8'h13: r1 = r1 / r2;
+                8'h14: r1 = r1 & r2;
+                8'h15: r1 = r1 | r2;
+                8'h16: r1 = r1 ^ r2;
+                8'h17: r1 = ~r1;
                 default: ;
             endcase
         end
+
         endmodule
+
+        module mem_fetch(
+            input clk,
+            input en,
+            input reg [7:0] data_in,
+            output reg [7:0] data_out,
+            output reg [7:0] addr,
+            output reg [7:0] dest
+            );
+            endmodule
