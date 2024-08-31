505ac10cc5137fd9efcc351b6d97ef1504c42580
Prithvi Vishak <prithvivishak@gmail.com>
2024-08-31 12:48:16 +0530

    Start from scratch

--- 8bit.sv
+++ 8bit.sv
@@ -9,7 +9,8 @@ module eightbit(
     );
 
     reg [7:0] a, b, pc, inst;
-    reg [2:0] pipe;
+    reg [3:0] inst_stage;
+    reg alu_do;
 
     initial begin
         a = 8'h00;
@@ -17,76 +18,33 @@ module eightbit(
         pc = 8'h00;
         inst = 8'h00;
         pipe = 3'h0;
+        inst_stage = 4'h00;
         we = 1'b0;
+        alu_do = 0;
         addr = pc;
     end
 
-    always @ (posedge clk) begin
-        case (pipe)
-            3'h0: begin
-                pipe <= 3'h1;
-                inst <= data_in;
-            end
-            3'h1: begin
-                case (inst)
-                    // Move word at address in pc + 0x01 to a
-                    8'h01: addr[7:0] <= pc[7:0] + 8'h01;
+    alu ALU(.r1(a), .r2(b), .inst(inst), .en(alu_do));
 
-                    // Move a to address at pc + 0x01
-                    8'h02: begin
-                        addr[7:0] = pc[7:0] + 8'h01;
-                        we = 1;
-                        data_out[7:0] = a[7:0];
-                    end
-                    // Move pc to a.
-                    8'h03: a[7:0] = pc[7:0];
-
-                    // Move a to pc.
-                    8'h04: pc[7:0] = a[7:0];
-
-                    // Move b to a.
-                    8'h05: a[7:0] = b[7:0];
-
-                    // Move a to b.
-                    8'h06: b[7:0] = a[7:0];
-
-                    8'h10: a = a + b;
-                    8'h11: a = a - b;
-                    8'h12: a = a * b;
-                    8'h13: a = a / b;
-                    8'h14: a = a & b;
-                    8'h15: a = a | b;
-                    8'h16: a = a ^ b;
-                    8'h17: a = ~a;
-
-                    default: ;
-                endcase
-                pipe = 3'h2;
-            end
-            3'h2: begin
-                // Move word at pc + 0x01 to a
-                if (inst[7:0] == 8'h01) begin
-                    addr[7:0] <= data_in[7:0];
-                    pipe[2:0] <= 3'h3;
-                end else begin
-                    if (inst[7:0] == 8'h02)
-                        pc[7:0] = pc[7:0] + 8'h01;
-
-                    pipe[2:0] <= 2'b0;
-                    we = 0;
-                    pc[7:0] = pc[7:0] + 8'h01;
-                    addr = pc;
-                end
-            end
-            3'h3: begin
-                pipe <= 3'h4;
-            end
-            3'h4: begin
-                a[7:0] = data_in[7:0];
-                pipe <= 3'h0;
-                pc[7:0] = pc[7:0] + 8'h02;
-                addr = pc;
-            end
-        endcase
-    end
     endmodule
+
+    module alu(
+        input reg [7:0] r1,
+        input reg [7:0] r2,
+        input reg [7:0] inst,
+        input en
+        );
+        always @ (posedge en) begin
+            case (instr[7:0])
+                8'h10: a = a + b;
+                8'h11: a = a - b;
+                8'h12: a = a * b;
+                8'h13: a = a / b;
+                8'h14: a = a & b;
+                8'h15: a = a | b;
+                8'h16: a = a ^ b;
+                8'h17: a = ~a;
+                default: ;
+            endcase
+        end
+        endmodule
