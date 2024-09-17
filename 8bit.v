25334fc3f733511034fe7728eef3ee00405ffc6a
Prithvi Vishak <prithvivishak@gmail.com>
2024-09-17 19:21:57 +0530

    Get fetch module working

--- 8bit.v
+++ 8bit.v
@@ -1,49 +1,80 @@
 `default_nettype none
 
 module eightbit(
-    input reg clk,
-    input reg [7:0] data_in,
+    input clk,
+    input [7:0] data_in,
     output reg [7:0] addr,
     output reg [7:0] data_out,
     output reg we
 );
 
 reg [7:0] a, b, pc, inst;
-reg [3:0] inst_stage;
-reg alu_do;
+reg fetch_en;
+wire fetch_ready, fetch_we;
+wire [7:0] fetch_inst, fetch_addr, fetch_pc;
 
-alu ALU(.r1(a), .r2(b), .inst(inst), .en(alu_do));
+fetch pipe_fetch(.clk(clk), .en(fetch_en), .pc_in(pc), .data_in(data_in), .addr(fetch_addr), .pc(fetch_pc), .inst(fetch_inst), .we(fetch_we), .ready(fetch_ready));
 
 initial begin
     pc = 8'h00;
     inst = 8'h00;
-    inst_stage = 4'h0;
-    we = 1'b0;
-    alu_do = 0;
-    addr = pc;
+    fetch_en = 0;
+end
+
+always @ (posedge clk) begin
+    if (~fetch_en) begin
+        fetch_en <= 1;
+    end
+
+    if (fetch_ready) begin
+        inst <= fetch_inst;
+        fetch_en <= 0;
+    end
+
+    if (fetch_en) begin
+        addr <= fetch_addr;
+        we <= fetch_we;
+        pc <= fetch_pc;
+    end
 end
 
 endmodule
 
-module alu(
-    input en,
-    input reg [7:0] inst,
-    input reg [7:0] r2,
-    output reg [7:0] r1
+module fetch(
+    input reg clk,
+    input reg en,
+    input reg [7:0] data_in,
+    input reg [7:0] pc_in,
+    output reg [7:0] addr,
+    output reg [7:0] pc,
+    output reg [7:0] inst,
+    output reg we,
+    output reg ready
 );
 
+reg [3:0] stage;
+
 always @ (posedge en) begin
-    case (inst)
-        8'h10: r1 = r1 + r2;
-        8'h11: r1 = r1 - r2;
-        8'h12: r1 = r1 * r2;
-        8'h13: r1 = r1 / r2;
-        8'h14: r1 = r1 & r2;
-        8'h15: r1 = r1 | r2;
-        8'h16: r1 = r1 ^ r2;
-        8'h17: r1 = ~r1;
-        default: ;
-    endcase
+    stage <= 0;
+    ready <= 0;
+    addr <= pc_in;
+    pc <= pc_in;
+    we <= 0;
 end
 
-endmodule
+always @ (posedge clk) begin
+    if (~ready)
+        case (stage)
+            4'h0: begin
+                inst = data_in;
+                addr = pc + 1;
+                stage = 1;
+            end
+            4'h1: begin
+                pc = pc + 2;
+                ready = 1;
+            end
+        endcase
+    end
+
+    endmodule
--- 8bit_tb.v
+++ 8bit_tb.v
@@ -3,7 +3,7 @@ module eightbit_tb();
 reg [7:0] mem [0:255];
 reg clk;
 reg [7:0] addr, data_out, data_in;
-reg we;
+wire we;
 
 eightbit eb(
     .clk(clk),
@@ -11,50 +11,50 @@ eightbit eb(
     .data_in(data_out),
     .data_out(data_in),
     .we(we)
-    );
+);
 
-    task pulseClk;
-        #1 clk = ~clk;
-    endtask
+task pulseClk;
+    #1 clk = ~clk;
+endtask
 
-    integer i;
-    initial begin
-        // load addends into memory
-        mem[8'hfe] = 8'h03;
-        mem[8'hff] = 8'h05;
+integer i;
+initial begin
+    // load addends into memory
+    mem[8'hfe] = 8'h03;
+    mem[8'hff] = 8'h05;
 
-        // Load 3 at 0xfe into a
-        mem[8'h00] = 8'h01;
-        mem[8'h01] = 8'hfe;
-        // Move a to b
-        mem[8'h02] = 8'h06;
-        // Load 5 at 0xff into a
-        mem[8'h03] = 8'h01;
-        mem[8'h04] = 8'hff;
-        // Add a and b
-        mem[8'h05] = 8'h10;
-        // Move a to 0xff
-        mem[8'h06] = 8'h02;
-        mem[8'h07] = 8'hff;
-        // Move 0x0a to a
-        mem[8'h08] = 8'h01;
-        mem[8'h09] = 8'h0a;
-        // Move pc to a
-        mem[8'h0a] = 8'h03;
+    // Load 3 at 0xfe into a
+    mem[8'h00] = 8'h01;
+    mem[8'h01] = 8'hfe;
+    // Move a to b
+    mem[8'h02] = 8'h06;
+    // Load 5 at 0xff into a
+    mem[8'h03] = 8'h01;
+    mem[8'h04] = 8'hff;
+    // Add a and b
+    mem[8'h05] = 8'h10;
+    // Move a to 0xff
+    mem[8'h06] = 8'h02;
+    mem[8'h07] = 8'hff;
+    // Move 0x0a to a
+    mem[8'h08] = 8'h01;
+    mem[8'h09] = 8'h0a;
+    // Move pc to a
+    mem[8'h0a] = 8'h03;
 
-        $dumpfile("8bit.vcd");
-        $dumpvars(0, eightbit_tb);
-        $dumpvars(0, mem[255]);
-        clk = 0;
+    $dumpfile("8bit.vcd");
+    $dumpvars(0, eightbit_tb);
+    $dumpvars(0, mem[255]);
+    clk = 0;
 
-        for (i = 0; i < 400; i = i + 1)
-            pulseClk();
-    end
+    for (i = 0; i < 400; i = i + 1)
+        pulseClk();
+end
 
-    always @(posedge clk) begin
-        if (we)
-            mem[addr] = data_in;
-        data_out = mem[addr];
-    end
+always @(posedge clk) begin
+    if (we)
+        mem[addr] = data_in;
+    data_out = mem[addr];
+end
 
-    endmodule;
+endmodule;
