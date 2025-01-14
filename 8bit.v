4c369b02b7529921914011c3b2c370a3e0da3711
Prithvi Vishak <prithvivishak@gmail.com>
2025-01-14 11:06:16 +0530

    Start from scratch with basic jump instruction

--- 8bit.v
+++ 8bit.v
@@ -9,72 +9,29 @@ module eightbit(
 );
 
 reg [7:0] a, b, pc, inst;
-reg fetch_en;
-wire fetch_ready, fetch_we;
-wire [7:0] fetch_inst, fetch_addr, fetch_pc;
-
-fetch pipe_fetch(.clk(clk), .en(fetch_en), .pc_in(pc), .data_in(data_in), .addr(fetch_addr), .pc(fetch_pc), .inst(fetch_inst), .we(fetch_we), .ready(fetch_ready));
 
 initial begin
     pc = 8'h00;
-    inst = 8'h00;
-    fetch_en = 0;
 end
 
 always @ (posedge clk) begin
-    if (~fetch_en) begin
-        fetch_en <= 1;
-    end
-
-    if (fetch_ready) begin
-        inst <= fetch_inst;
-        fetch_en <= 0;
-    end
-
-    if (fetch_en) begin
-        addr <= fetch_addr;
-        we <= fetch_we;
-        pc <= fetch_pc;
-    end
+    case (stage)
+        4'h0: begin
+            we = 0;
+            addr = pc;
+            stage <= stage + 1;
+        end
+        4'h1: begin
+            inst = data_in;
+            case (inst[7:6])
+                2'b00: pc <= inst[5:0];
+                2'b01: a <= a + b;
+            endcase
+            stage <= 0;
+        end
+        default:
+            stage <= 0;
+    endcase
 end
 
 endmodule
-
-module fetch(
-    input reg clk,
-    input reg en,
-    input reg [7:0] data_in,
-    input reg [7:0] pc_in,
-    output reg [7:0] addr,
-    output reg [7:0] pc,
-    output reg [7:0] inst,
-    output reg we,
-    output reg ready
-);
-
-reg [3:0] stage;
-
-always @ (posedge en) begin
-    stage <= 0;
-    ready <= 0;
-    addr <= pc_in;
-    pc <= pc_in;
-    we <= 0;
-end
-
-always @ (posedge clk) begin
-    if (~ready)
-        case (stage)
-            4'h0: begin
-                inst = data_in;
-                addr = pc + 1;
-                stage = 1;
-            end
-            4'h1: begin
-                pc = pc + 2;
-                ready = 1;
-            end
-        endcase
-    end
-
-    endmodule
--- 8bit_tb.v
+++ 8bit_tb.v
@@ -14,40 +14,21 @@ eightbit eb(
 );
 
 task pulseClk;
-    #1 clk = ~clk;
+    #0.5 clk = ~clk;
+    #0.5 clk = ~clk;
 endtask
 
 integer i;
 initial begin
-    // load addends into memory
-    mem[8'hfe] = 8'h03;
-    mem[8'hff] = 8'h05;
-
-    // Load 3 at 0xfe into a
-    mem[8'h00] = 8'h01;
-    mem[8'h01] = 8'hfe;
-    // Move a to b
-    mem[8'h02] = 8'h06;
-    // Load 5 at 0xff into a
-    mem[8'h03] = 8'h01;
-    mem[8'h04] = 8'hff;
-    // Add a and b
-    mem[8'h05] = 8'h10;
-    // Move a to 0xff
-    mem[8'h06] = 8'h02;
-    mem[8'h07] = 8'hff;
-    // Move 0x0a to a
-    mem[8'h08] = 8'h01;
-    mem[8'h09] = 8'h0a;
-    // Move pc to a
-    mem[8'h0a] = 8'h03;
+    // Load jump instruction
+    mem[0] = 8'b00000101;
 
     $dumpfile("8bit.vcd");
     $dumpvars(0, eightbit_tb);
     $dumpvars(0, mem[255]);
     clk = 0;
 
-    for (i = 0; i < 400; i = i + 1)
+    for (i = 0; i < 4; i = i + 1)
         pulseClk();
 end
 
