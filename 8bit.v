bea5e4bba470aabc681f827b6ca516075af66ae9
Prithvi Vishak <prithvivishak@gmail.com>
2025-01-14 15:47:35 +0530

    Create separate fetch module

--- 8bit.v
+++ 8bit.v
@@ -9,29 +9,20 @@ module eightbit(
 );
 
 reg [7:0] a, b, pc, inst;
+reg fetch_en, fetch_ready;
+
+fetch Fetch (
+    .fetch_en(fetch_en),
+    .clk(clk),
+    .data_in(data_in),
+    .pc(pc),
+    .addr(addr),
+    .inst_out(inst)
+);
 
 initial begin
     pc = 8'h00;
-end
-
-always @ (posedge clk) begin
-    case (stage)
-        4'h0: begin
-            we = 0;
-            addr = pc;
-            stage <= stage + 1;
-        end
-        4'h1: begin
-            inst = data_in;
-            case (inst[7:6])
-                2'b00: pc <= inst[5:0];
-                2'b01: a <= a + b;
-            endcase
-            stage <= 0;
-        end
-        default:
-            stage <= 0;
-    endcase
+    fetch_en = 1;
 end
 
 endmodule
---
+++ fetch.v
@@ -0,0 +1,35 @@
+module fetch(
+    input fetch_en,
+    input clk,
+    input [7:0] data_in,
+    input [7:0] pc,
+    output reg [7:0] addr,
+    output reg [7:0] inst_out,
+    output reg fetch_ready
+);
+
+reg [1:0] stage;
+
+always @ (posedge fetch_en) begin
+    stage <= 0;
+    fetch_ready <= 0;
+end
+
+always @ (posedge clk) begin
+    if (fetch_en) begin
+        case (stage)
+            2'b00: begin
+                addr <= pc;
+                stage <= 1;
+            end
+            2'b01: begin
+                inst_out <= data_in;
+                fetch_ready <= 1;
+                stage <= 2;
+            end
+            default: fetch_ready <= fetch_ready;
+        endcase
+    end
+end
+
+endmodule
