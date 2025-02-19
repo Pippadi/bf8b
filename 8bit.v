6c1ae3dcfaec6adb462507e3cff232af079a02f9
Prithvi Vishak <prithvivishak@gmail.com>
2025-02-19 22:40:26 +0530

    Do writeback

--- 8bit.v
+++ 8bit.v
@@ -43,6 +43,8 @@ decode Decode (
 
 reg exec_en;
 reg exec_ready;
+reg [1:0] exec_op;
+reg [4:0] exec_addr_in;
 reg [7:0] exec_val1_in;
 reg [7:0] exec_val2_in;
 reg [7:0] exec_val_out;
@@ -54,16 +56,30 @@ exec Execute (
     .en(exec_en),
     .rst(pipeline_flush),
     .clk(clk),
-    .op(decode_inst_type),
+    .op(exec_op),
     .val1(exec_val1_in),
     .val2(exec_val1_in),
-    .addr_in(decode_addr[4:0]),
+    .addr_in(exec_addr_in),
     .mem_data_in(data_in),
     .val_out(exec_val_out),
     .mem_addr(exec_addr),
     .mem_data_out(exec_data_out),
     .we(exec_we),
     .ready(exec_ready)
+);
+
+reg wb_srcdst, wb_ready, wb_en;
+reg [1:0] wb_op;
+
+writeback Writeback(
+    .en(wb_en),
+    .clk(clk),
+    .op(exec_op),
+    .srcdst(wb_srcdst),
+    .val(exec_val_out),
+    .a(a),
+    .b(b),
+    .ready(wb_ready)
 );
 
 initial begin
@@ -78,12 +94,26 @@ always @ (posedge fetch_ready) begin
     pipeline_flush = 0;
 end
 
+always @ (posedge exec_ready) begin
+    exec_en <= 0;
+    wb_en <= 1;
+    wb_op <= exec_op;
+end
+
+always @ (posedge wb_ready) begin
+    wb_en = 0;
+end
+
 always @ (posedge clk) begin
     if (fetch_en) begin
         addr <= fetch_addr;
     end
 
     if (decode_ready & ~exec_en & ~fetch_en) begin
+        exec_op <= decode_inst_type;
+        exec_addr_in <= decode_addr[4:0];
+        wb_srcdst <= decode_srcdst;
+
         case (decode_inst_type)
             2'b00: begin
                 pc <= {2'b00, decode_addr};
@@ -96,8 +126,6 @@ always @ (posedge clk) begin
             2'b10: begin
                 exec_val1_in <= (decode_srcdst) ? a : b;
                 exec_en <= 1;
-            end
-            2'b11: begin
             end
         endcase
     end
---
+++ writeback.v
@@ -0,0 +1,34 @@
+module writeback(
+    input en,
+    input clk,
+    input [1:0] op,
+    input srcdst,
+    input [7:0] val,
+    output reg [7:0] a,
+    output reg [7:0] b,
+    output reg ready
+);
+
+always @ (posedge en) begin
+    ready <= 0;
+end
+
+always @ (posedge clk) begin
+    if (en) begin
+        if (op == 2'b01) begin
+            if (srcdst) begin
+                a <= val;
+            end
+            else begin
+                b <= val;
+            end
+            ready <= 1;
+        end
+        else begin
+            a <= a;
+            b <= b;
+        end
+    end
+end
+
+endmodule
