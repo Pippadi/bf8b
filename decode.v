e7a4ffff3da5256d80d38bdd88b995430ed52e2b
Prithvi Vishak <prithvivishak@gmail.com>
2025-03-13 10:41:21 +0530

    Switch to non-blocking assigns in decode

--- decode.v
+++ decode.v
@@ -2,7 +2,7 @@ module decode(
     input en,
     input clk,
     input [7:0] inst,
-    output reg [1:0] inst_type,
+    output reg [1:0] op,
     output reg srcdst,
     output reg [5:0] addr,
     output reg ready
@@ -14,23 +14,19 @@ end
 
 always @ (posedge clk) begin
     if (en) begin
-        inst_type = inst[7:6];
-        case (inst_type)
-            2'b00: addr = inst[5:0];
-            2'b01: begin
-                addr = {1'b0, inst[4:0]};
-                srcdst = inst[5];
-            end
-            2'b10: begin
-                addr = {1'b0, inst[4:0]};
-                srcdst = inst[5];
-            end
+        op <= inst[7:6];
+        case (inst[7:6])
+            2'b00: addr <= inst[5:0];
             2'b11: begin
-                addr = 0;
-                srcdst = inst[5];
+                addr <= 0;
+                srcdst <= inst[5];
+            end
+            default: begin
+                addr <= {1'b0, inst[4:0]};
+                srcdst <= inst[5];
             end
         endcase
-        ready = 1;
+        ready <= 1;
     end
 end
 
