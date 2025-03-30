1b265ac50d4eb434398360b9f66a70362a5d2308
Prithvi Vishak <prithvivishak@gmail.com>
2025-03-30 10:24:35 +0530

    Begin rewriting cache module [incomplete]
    Create shift register with individual enables.

--- cache.v
+++ cache.v
@@ -1,3 +1,45 @@
+module shift_reg
+(
+    parameter LENGTH = 8,
+    parameter WIDTH = 8
+)
+(
+    input rst,
+    input clk,
+    input [LENGTH-1:0] en,
+    input [WIDTH-1:0] d,
+    output reg [LENGTH * WIDTH - 1:0] q_packed
+);
+
+reg [WIDTH-1:0] q [0:LENGTH-1];
+
+integer i;
+always @ (*) begin
+    for (i = 0; i < LENGTH; i = i + 1) begin
+        q_packed[WIDTH*i +: WIDTH] = q[i];
+    end
+end
+
+integer j;
+wire [WIDTH-1:0] d [LENGTH-1:0];
+assign ds = {d, q[LENGTH-1:1]};
+
+always @ (posedge clk or posedge rst) begin
+    if (rst) begin
+        for (j = 0; j < LENGTH; j = j + 1) begin
+            q[j] <= {WIDTH{1'b1}};
+        end
+    end
+    else begin
+        for (j = 0; j < LENGTH; j = j + 1) begin
+            if (en[j])
+                q[j] <= ds[j];
+        end
+    end
+end
+
+endmodule
+
 module cache
 #(
     parameter ADDR_WIDTH = 8,
@@ -14,67 +56,38 @@ module cache
 );
 
 reg [DATA_WIDTH-1:0] data_reg;
+reg [CELL_CNT-1:0] enables;
 
-reg [ADDR_WIDTH-1:0] addrs [0:CELL_CNT-1];
-reg [DATA_WIDTH-1:0] datas [0:CELL_CNT-1];
-reg [$clog2(CELL_CNT)-1:0] ages [0:CELL_CNT-1];
-reg [CELL_CNT-1:0] initialized;
+wire [(ADDR_WIDTH+DATA_WIDTH)*CELL_CNT-1:0] reg_data_packed;
+wire [ADDR_WIDTH+DATA_WIDTH-1:0] reg_data [0:CELL_CNT-1];
+
+integer i;
+always @ (*) begin
+    for (i = 0; i < 16; i = i + 1) begin
+        reg_file[i] = reg_data_packed[8*i +: 8];
+    end
+end
+
+shift_reg #(
+    .LENGTH(CELL_CNT),
+    .WIDTH(ADDR_WIDTH+DATA_WIDTH)
+) ShiftReg (
+    .rst(rst),
+    .clk(clk),
+    .en(enables),
+    .d(),
+    .qs(reg_data_packed)
+);
 
 assign data = (we) ? 'hz : data_reg;
 
-integer i;
+integer j;
 integer hitIdx;
-integer eldest;
-
-always @ (posedge clk or posedge rst) begin
-    if (rst) begin
-        hit = 0;
-        initialized = 0;
-        data_reg = 0;
-        for (i = 0; i < CELL_CNT; i = i + 1) begin
-            addrs[i] = 0;
-            datas[i] = 0;
-            ages[i] = 0;
-        end
-    end
-    else begin
-        hit = 0;
-        for (i = 0; i < CELL_CNT; i = i + 1) begin
-            if (addrs[i] == addr && initialized[i]) begin
-                data_reg = datas[i];
-                hit = 1;
-                hitIdx = i;
-            end
-        end
-
-        if (we) begin
-            if (hit) begin
-                datas[hitIdx] = data;
-                ages[hitIdx] = 0;
-            end
-            else begin
-                eldest = 0;
-                for (i = 0; i < CELL_CNT; i = i + 1) begin
-                    if (initialized != {CELL_CNT{1'b1}}) begin
-                        if (!initialized[i])
-                            eldest = i;
-                    end
-                    else begin
-                        if (ages[i] > ages[eldest])
-                            eldest = i;
-                    end
-                end
-
-                addrs[eldest] = addr;
-                datas[eldest] = data;
-                ages[eldest] = 0;
-                initialized[eldest] = 1;
-
-                for (i = 0; i < CELL_CNT; i = i + 1) begin
-                    if (i != eldest && initialized[i])
-                        ages[i] = ages[i] + 1;
-                end
-            end
+always @ (posedge clk) begin
+    for (j = 0; j < CELL_CNT; j = j + 1) begin
+        if (addr == reg_data[j][ADDR_WIDTH+DATA_WIDTH:DATA_WIDTH]) begin
+            data_reg <= reg_data[j][DATA_WIDTH-1:0];
+            hitIdx = j;
         end
     end
 end
