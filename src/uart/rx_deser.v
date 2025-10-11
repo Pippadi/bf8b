d8f34b06fec1dd2feca68be72ef04829d2c390bb
Prithvi Vishak <prithvivishak@gmail.com>
2025-10-11 19:29:15 +0530

    Discard start bit in RX deserialization

--- src/uart/rx_deser.v
+++ src/uart/rx_deser.v
@@ -11,13 +11,15 @@ module rx_deserializer
     output reg latch_data
 );
 
+localparam BITS_TO_READ = 9; // 1 start, 8 data (discard stop)
+
 reg [$clog2(RX_CLKS_PER_BIT)-1:0] clk_count;
-reg [$clog2(10)-1:0] bit_idx;
+reg [$clog2(BITS_TO_READ)-1:0] bit_idx;
 reg line_active;
 reg data_latched;
 
 wire should_latch_data;
-assign should_latch_data = (clk_count == (RX_CLKS_PER_BIT-1)/2) && (bit_idx < 8);
+assign should_latch_data = (clk_count == (RX_CLKS_PER_BIT-1)/2) && (bit_idx < BITS_TO_READ);
 
 always @ (posedge clk) begin
     if (rst) begin
@@ -36,31 +38,27 @@ always @ (posedge clk) begin
         end
 
         if (deser_clk_posedge & line_active) begin
-            if (bit_idx == 9) begin
-                line_active <= 'b0;
-                clk_count <= 'b0;
-                bit_idx <= 'b0;
-                latch_data <= 'b0;
-            end else begin
-                if (should_latch_data) begin
-                    data <= {rx, data[7:1]};
-                    clk_count <= clk_count + 1'b1;
-                end
-
-                if (clk_count == RX_CLKS_PER_BIT-1) begin
-                    clk_count <= 'b0;
-                    bit_idx <= bit_idx + 1'b1;
-                end else
-                    clk_count <= clk_count + 1'b1;
+            if (should_latch_data) begin
+                data <= {rx, data[7:1]};
+                clk_count <= clk_count + 1'b1;
             end
-        end
 
-        if (bit_idx == 9 && !data_latched) begin
-            latch_data <= 1'b1;
-            data_latched <= 1'b1;
-        end else
-            latch_data <= 1'b0;
+            if (clk_count == RX_CLKS_PER_BIT-1) begin
+                clk_count <= 'b0;
+                bit_idx <= bit_idx + 1'b1;
+            end else
+                clk_count <= clk_count + 1'b1;
+        end
     end
+
+    if (bit_idx == BITS_TO_READ && !data_latched) begin
+        latch_data <= 1'b1;
+        data_latched <= 1'b1;
+        line_active <= 'b0;
+        clk_count <= 'b0;
+        bit_idx <= 'b0;
+    end else
+        latch_data <= 1'b0;
 end
 
 endmodule
